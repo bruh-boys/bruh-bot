0cda4ee88f9b6645db1a1ee2ffdfe92c0be5dcd6
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-03-06 14:10:00 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,28 +27,25 @@ SMS: *sms +573221243431""", color=0x00ff00)
 @bot.command()
 async def sms(ctx,arg):
 
-     validate = re.findall("\+?[\d]{10,14}", arg)
-     if not validate:
-	 embed10 = discord.Embed(title ="you didn't pass with a +, or is not a number, or did you pass it with spaces, example: *sms +22123456789", color=0x00ff01)
-	 await ctx.send(embed=embed10)
-	 return
-     #print(validate[0]) 
-     embed = discord.Embed(title ="the attack takes between takes 2 minutes", color=0x00ff00, timestamp=datetime.datetime.utcnow())
-     await ctx.send(embed=embed)
-     os.system(f"cd quack ; python3 quack --tool SMS --target  {validate[0]} --threads 60 --timeout 90")	
-     embed2 = discord.Embed(title ="attack completed! :skull:" ,color=0x00ff00,timestamp=datetime.datetime.utcnow())
-     await ctx.send(embed=embed2)
+	validate = re.findall("\+?[\d]{10,14}", arg)
+	if not validate:
+		embed10 = discord.Embed(title ="you didn't pass with a +, or is not a number, or did you pass it with spaces, example: *sms +22123456789", color=0x00ff01)
+		await ctx.send(embed=embed10)
+		return
+	#print(validate[0])
+	embed = discord.Embed(title ="the attack takes between takes 2 minutes", color=0x00ff00, timestamp=datetime.datetime.utcnow())
+	await ctx.send(embed=embed)
+	os.system(f"cd quack ; python3 quack --tool SMS --target  {validate[0]} --threads 60 --timeout 90")	
+	embed2 = discord.Embed(title ="attack completed! :skull:" ,color=0x00ff00,timestamp=datetime.datetime.utcnow())
+	await ctx.send(embed=embed2)
 
 @bot.command()
 async def email(ctx,arg):
-     embed4 = discord.Embed(title ="spam sent! takes 1 minute and 30 seconds", color=000000)
-     await ctx.send(embed=embed4)
-     os.system(f"python3 impulse.py --method EMAIL --time 90 --target {arg}")
-     embed5 = discord.Embed(title ="atack completed!", color=0x00ff00, timestamp=datetime.datetime.utcnow())
-     await ctx.send(embed=embed5)
+    embed4 = discord.Embed(title ="spam sent! takes 1 minute and 30 seconds", color=000000)
+    await ctx.send(embed=embed4)
+    os.system(f"python3 impulse.py --method EMAIL --time 90 --target {arg}")
+    embed5 = discord.Embed(title ="atack completed!", color=0x00ff00, timestamp=datetime.datetime.utcnow())
+    await ctx.send(embed=embed5)
 
-      
+
 bot.run(SI)
-
-
-
