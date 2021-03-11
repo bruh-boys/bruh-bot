6c74a4d95ec3a655329440acf6bbadd0f4ef665f
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-03-10 20:35:17 -0500

    a little cooldown

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ bot = commands.Bot(command_prefix='*', description="Test bot")
 # Event
 @bot.event
 async def on_ready():
-   await bot.change_presence(activity=discord.Game(name="*info"))
+   await bot.change_presence(activity=discord.Streaming(name="*info", url="https://twitch.tv/monda"))
    print("\033[4;35m"+"bot on")
 
 
@@ -32,20 +32,21 @@ async def sms(ctx,arg):
 		embed10 = discord.Embed(title ="you didn't pass with a +, or is not a number, or did you pass it with spaces, example: *sms +22123456789", color=0x00ff01)
 		await ctx.send(embed=embed10)
 		return
-	#print(validate[0])
-	embed = discord.Embed(title ="the sms attack takes between takes 2 minutes", color=0x00ff00, timestamp=datetime.datetime.utcnow())
+	#print(validate[0]) 
+	embed = discord.Embed(title ="the spam takes between takes 2 minutes", color=0x00ff00, timestamp=datetime.datetime.utcnow())
 	await ctx.send(embed=embed)
 	os.system(f"cd quack ; python3 quack --tool SMS --target  {validate[0]} --threads 60 --timeout 90")	
-	embed2 = discord.Embed(title ="sms attack completed! :skull:" ,color=0x00ff00,timestamp=datetime.datetime.utcnow())
+	embed2 = discord.Embed(title ="sms spam completed! :skull:" ,color=0x00ff00,timestamp=datetime.datetime.utcnow())
 	await ctx.send(embed=embed2)
 
 @bot.command()
 async def email(ctx,arg):
-    embed4 = discord.Embed(title ="Email spam sent! takes 1 minute and 30 seconds", color=000000)
+    embed4 = discord.Embed(title ="spam sent! takes 1 minute ", color=000000)
     await ctx.send(embed=embed4)
-    os.system(f"python3 impulse.py --method EMAIL --time 90 --target {arg}")
-    embed5 = discord.Embed(title ="Email atack completed!", color=0x00ff00, timestamp=datetime.datetime.utcnow())
+    time.sleep(6)
+    os.system(f"python3 impulse.py --method EMAIL --time 60 --target {arg}")
+    embed5 = discord.Embed(title =" email spam completed!", color=0x00ff00, timestamp=datetime.datetime.utcnow())
     await ctx.send(embed=embed5)
 
-
+      
 bot.run(SI)
