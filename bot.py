78c3c1e7703fcda2959e84e7b5ee3b0689ee00c5
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-04-29 18:30:26 -0500

    cool changes

--- bot.py
+++ bot.py
@@ -5,23 +5,21 @@ import datetime
 import os
 import re
 
-bot = commands.Bot(command_prefix='*', description="Test bot")
-# Event
+bot = commands.Bot(command_prefix='*', description="sms and email spam bot!")
 
 
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Game(name="*info"))
-    print("\033[4;35m"+"bot on")
+    print("bot on")
 
 
 @bot.command()
 async def info(ctx):
-    embed3 = discord.Embed(title="""commands:
-
-EMAIL: *email test@test.com
-
-SMS: *sms +573221243431""", color=0x00ff00)
+    embed3 = discord.Embed(title="invitation link!",
+                           url="https://discord.com/oauth2/authorize?client_id=817060522067099679&permissions=51200&scope=bot", color=0x00ffff)
+    embed3.add_field(name="Email example:", value="*email hello@gmail.com")
+    embed3.add_field(name="SMS example:", value="*sms +3231231231")
     await ctx.send(embed=embed3)
 
 
@@ -31,10 +29,9 @@ async def sms(ctx, arg):
     validate = re.findall("\+?[\d]{10,14}", arg)
     if not validate:
         embed10 = discord.Embed(
-            title="you didn't pass with a +, or is not a number, or did you pass it with spaces, example: *sms +22123456789", color=0x00ff01)
+            title="you didn't pass with a +, or is not a number, or did you pass it with spaces, example: *sms +22123456789", color=330000)
         await ctx.send(embed=embed10)
         return
-    # print(validate[0])
     embed = discord.Embed(title="the spam takes between takes 2 minutes",
                           color=0x00ff00, timestamp=datetime.datetime.utcnow())
     await ctx.send(embed=embed)
@@ -47,17 +44,19 @@ async def sms(ctx, arg):
 
 @bot.command()
 async def email(ctx, arg):
-    validateEmail = re.findall("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", arg)
+    validateEmail = re.findall(
+        "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", arg)
     if not validateEmail:
         embed11 = discord.Embed(
-            title="you didn't pass a valid email, example: *email bruh@gmail.com", color=0x00ff01)
+            title="you didn't pass a valid email, example: *email hello@gmail.com", color=330000)
         await ctx.send(embed=embed11)
         return
 
     embed4 = discord.Embed(title="spam sent! takes 1 minute ", color=000000)
     await ctx.send(embed=embed4)
     os.system("rm tools/EMAIL/sender.json")
-    os.system(f"python3 impulse.py --method EMAIL --time 60 --target {validateEmail[0]}")
+    os.system(
+        f"python3 impulse.py --method EMAIL --time 60 --target {validateEmail[0]}")
     embed5 = discord.Embed(title=" email spam completed!",
                            color=0x00ff00, timestamp=datetime.datetime.utcnow())
     await ctx.send(embed=embed5)
