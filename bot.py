79c75e4970c6f4b9a6eacdd07898e13c5bb2ef79
pai <hachikopolarcito321@gmail.com>
2021-05-04 13:15:42 -0500

    new stuffs

--- bot.py
+++ bot.py
@@ -30,16 +30,18 @@ async def sms(ctx, arg):
     validate = re.findall("\+?[\d]{10,14}", arg)
     if not validate:
         embed10 = discord.Embed(
-            title="you didn't pass with a +, or is not a number, or did you pass it with spaces, example: *sms +22123456789",color=330000)
+            title="you didn't pass with a +, or is not a number, or did you pass it with spaces, example: *sms +22123456789", color=330000)
         await ctx.send(embed=embed10)
         return
     embed = discord.Embed(title="the spam takes between takes 2 minutes",
                           color=0x00ff00, timestamp=datetime.datetime.utcnow())
     await ctx.send(embed=embed)
+    await bot.change_presence(activity=discord.Game(name=f"busy | *info | in {len(bot.guilds)} servers"))
     os.system(
         f"cd quack ; python3 quack --tool SMS --target  {validate[0]} --threads 60 --timeout 90")
     embed2 = discord.Embed(title="sms spam completed! :skull:",
                            color=0x00ff00, timestamp=datetime.datetime.utcnow())
+    await bot.change_presence(activity=discord.Game(name=f"ready | *info | in {len(bot.guilds)} servers"))
     await ctx.send(embed=embed2)
 
 
@@ -55,11 +57,13 @@ async def email(ctx, arg):
 
     embed4 = discord.Embed(title="spam sent! takes 1 minute ", color=000000)
     await ctx.send(embed=embed4)
+    await bot.change_presence(activity=discord.Game(name=f"busy | *info | in {len(bot.guilds)} servers"))
     os.system("rm tools/EMAIL/sender.json")
     os.system(
         f"python3 impulse.py --method EMAIL --time 60 --target {validateEmail[0]}")
     embed5 = discord.Embed(title=" email spam completed!",
                            color=0x00ff00, timestamp=datetime.datetime.utcnow())
+    await bot.change_presence(activity=discord.Game(name=f"ready | *info | in {len(bot.guilds)} servers"))
     await ctx.send(embed=embed5)
 
 
